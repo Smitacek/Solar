08033fc6cf3196c02b6536483cf6631aa7c55b75
jakubsmity <jakub.smetala@seznam.cz>
2024-10-10 15:12:29 +0200

    initial commit

--- serialPort.py
+++ serialPort.py
@@ -4,7 +4,38 @@ import time
 import serial
 from serial import Serial
 
-
+queries_and_responses = [
+    ['QPI', 'PI<NN>'],
+    ['QID', 'ABCDEEFFGXXXXX'],
+    ['QVFW', 'VERFW:<NNNNN.NN>'],
+    ['QVFW2', 'VERFW2:<NNNNN.NN>'],
+    ['QMD', 'TTTTTTTTTTTTTTTWWWWWWW KK P/P MMM NNN RR BB.B'],
+    ['QPIRI', 'BBB.B FF.F III.I EEE.E DDD.D AA.A GGG.G R MM T'],
+    ['QPIGS', 'MMM.M CBBBBB HH.H CZZZ.Z LLL.L MMMMM NN.N QQQ.Q DDD KKK.K VVV.V SSS.S RRR.R XXX PPPPP EEEEE OOOOO UUU.U WWW.W YYY.Y TTT.T b7b6b5b4b3b2b1b0a0a1'],
+    ['QMOD', 'M'],
+    ['QPIWS', 'a0a1.....a62a127'],
+    ['QFLAG', 'ExxxDxxx'],
+    ['QT', 'YYYYMMDDHHMMSS'],
+    ['QET', 'NNNNNNNN'],
+    ['QEY', 'NNNNNNNN'],
+    ['QEM', 'NNNNNN'],
+    ['QED', 'NNNNNN'],
+    ['QEH', 'NNNNN'],
+    ['QGOV', 'HHH.H LLL.L'],
+    ['QGOF', 'FF.F GG.G'],
+    ['QOPMP', 'LLLLL'],
+    ['QMPPTV', 'HHH LLL'],
+    ['QPVIPV', 'HHH LLL'],
+    ['QLST', 'LL'],
+    ['QTPR', 'LLL.L SSS.S TTT.T'],
+    ['QDI2', 'HH.H LL.L NNN'],
+    ['QDI', 'BBB.B CCC.C DD.D EE.E FFF.F GGG.G HH.H II.I JJJ KKK LLL MMM NNNNN OOO PP QQ RRR SS'],
+    ['QGLTV', 'HHH LLL'],
+    ['QCHGS', 'AA.A BB.B CC.C DD.D'],
+    ['QVFTR', 'HHH.H MMM.M LLL.L NNN.N ZZ.Z XX.X WW.W YY.Y AAA BBB'],
+    ['QPIHF', 'KK YYYYMMDDHHMMSS AAA.A BBB.B CCC.C DDD.D EEE.E FFF.F GGG.G HHH.H III.I JJ.J CKKK.K LLL MMM.M NNN.N OO.O PPP.P QQQ.Q <bn><cr>'],
+    ['QPICF', 'KK NN']
+]
 
 class SerialPort:
     BAUD_RATE = {'110': 110, '300': 300, '600': 600, '1200': 1200, '2400': 2400, '4800': 4800, '9600': 9600,
@@ -79,27 +110,50 @@ class RS232Protocol:
 
 class DecoderQ:
     def __init__(self, inverter_path):
-        self.open_file_inverter(inverter_path)
+        self.answer = None
         self._inverter = {}
+        self.open_file_inverter(inverter_path)
+
 
     def open_file_inverter(self, fila_path) -> None:
         with open(fila_path, 'r') as file:
             self._inverter = json.loads(file.read())
 
-    def _split(self):
-        raise NotImplementedError
+    def convert(self, sentence: str, answer: str):
+        _list_of_keys = []
+        _converted_values = {}
+        for key in self._inverter[sentence].keys():
+            _list_of_keys.append(key)
+        self.answer = answer.split(' ').copy()
+        try:
+            for number, value in enumerate(self.answer):
+                _converted_values[_list_of_keys[number]] = value + ' ' + self._inverter[sentence][_list_of_keys[number]]['unit']
+                print(_converted_values)
+        except IndexError:
+            pass
+
 
 
 if __name__ == '__main__':
+    decoder = DecoderQ('configuration/easun.json')
+    decoder.convert('QDI','230.0 50.0 0030 44.0 54.0 56.4 46.0 60 0 0 2 0 0 0 0 0 1 1 1 0 1 0 54.0 0 1 224')
     port = SerialPort({
         "name": "serial port",
         "port": "/dev/ttyUSB0",
         "baudrate": 2400,
         "timeout": 3
     })
-    decoder = DecoderQ('configuration/easun.json')
-    port.open()
-    port.write(b'QDI')
-    print(port.readline())
+
+    # port.open()
+    # port.write(b'QDI')
+    # print(port.readline())
+    for task in queries_and_responses:
+        print(task[0])
+        command = b'{task[0]}'
+        port.write(command)
+        print(task[1])
+        print(port.readline())
+        time.sleep(0.2)
+
 
     port.close()
